8467af058ecdd466eded50fbcf176b0a5fde3bd3
yoogesh <yoogie.ram@gmail.com>
2020-03-17 22:38:19 -0400

    Done with chapter 5 - 6

--- basic/Sources/basic/main.swift
+++ basic/Sources/basic/main.swift
@@ -1,9 +1,10 @@
-// This code involves basic swift programming
+// This code contains basic swift implementations
 
 // Declaring a variable using var 
 var myname = "Benz"
 var intvalue = 25
 var doublevalue = 23.2222222
+
 
 // Declaring the variable using Annotation 
 var a: Int
@@ -11,6 +12,13 @@ var A: Int
 
  a = 2
  A = 5 
+
+// Printing the relevant values 
+print("Value of \(a)", a)
+print("Value of A", A)
+print("Int value", intvalue)
+print("Double value", doublevalue)
+print("String Value", myname)
 
 
 // Declaring optionals - They can say, a value is present and so is the value or the value is not present 
@@ -41,13 +49,46 @@ else{
 
 
 
+// Tuples
+// Tuples can store any number of values, also values of different data types
+// Declare using () parenthesis
+
+var tupleone = (404, "Some error found")
+
+print("Printing full tuple", tupleone)
+
+// Access tuple elements by index starting at 0
+
+print("Tuple First element : Error code", tupleone.0)
+print("Tuple second element : Error Description", tupleone.1)
+
+// You can also declare the varibles using an name when declaring the tuple 
+
+var tupletwo = (Errorcode : 501, Description: "Some error")
+
+// Access the tuple elements using the var names 
+
+print("Tuple First element:", tupletwo.Errorcode)
+print("Tuple Second Element: ", tupletwo.Description)
 
 
 
-// Printing the relevant values 
 
-print("Value of \(a)", a)
-print("Value of A", A)
-print("Int value", intvalue)
-print("Double value", doublevalue)
-print("String Value", myname)
+// Constants 
+// initializing the constants doesnt allow the value of the varible to be modified in the run time 
+// declare it using the let keyword 
+
+let constA = 24
+// Type annotation of constants 
+
+let constB: Int = 20
+
+print("Const A", constA)
+print("Const B", constB)
+
+// changing the value of an constant 
+// constA = 21  - Throws an error - Change let to var to make it mutable
+
+// We have all basic arithmetic operations as outlined in other languages
+
+
