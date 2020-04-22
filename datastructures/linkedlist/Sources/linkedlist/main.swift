cb1154f751095a2195ea17a5eb1523de89f5f456
yoogesh <yoogie.ram@gmail.com>
2020-04-21 20:48:41 -0400

    DOne

--- datastructures/linkedlist/Sources/linkedlist/main.swift
+++ datastructures/linkedlist/Sources/linkedlist/main.swift
@@ -1,1 +1,13 @@
 // Writing the code to implement linked list 
+
+// Creating an node class
+
+public class Node{
+    var value: String
+    var next: Node?
+    weak var previous: Node?
+    init(value: String){
+        self.value = value 
+    }
+}
+
